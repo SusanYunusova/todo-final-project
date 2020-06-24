dfed9bf3f6c8138ea96bf3762730ced78788f6f9
Susan Yunusova <yunusova96.ys@gmail.com>
2020-06-24 17:33:34 +0400

    Initial commit

---
+++ src/test/java/az/ibatech/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ibatech.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
