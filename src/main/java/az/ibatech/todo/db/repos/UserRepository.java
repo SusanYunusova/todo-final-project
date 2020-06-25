88e78d1393b5bbbe7be88aae85b36e7a6d80d8e9
Susan Yunusova <yunusova96.ys@gmail.com>
2020-06-25 18:17:00 +0400

    1)jdbc connection done

--- src/main/java/az/ibatech/todo/db/repos/UserRepository.java
+++ src/main/java/az/ibatech/todo/db/repos/UserRepository.java
@@ -2,9 +2,10 @@ package az.ibatech.todo.db.repos;
 
 import az.ibatech.todo.db.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByIdUser(long idUser);
 
