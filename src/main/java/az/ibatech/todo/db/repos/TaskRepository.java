d4ae47461f39f76aa7d6a1bbe5ce2db6f82ab736
Susan Yunusova <yunusova96.ys@gmail.com>
2020-06-26 21:06:52 +0400

    1)Task Repository added

--- src/main/java/az/ibatech/todo/db/repos/TaskRepository.java
+++ src/main/java/az/ibatech/todo/db/repos/TaskRepository.java
@@ -1,4 +1,13 @@
 package az.ibatech.todo.db.repos;
 
-public class TaskRepository {
+import az.ibatech.todo.db.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface  TaskRepository extends JpaRepository<Task,Long> {
+    Optional<Task> findByIdTask(long idTask);
+
 }
