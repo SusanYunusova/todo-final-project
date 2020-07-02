4261b9c9a4c915d39dc09ffc89b7a162d31481aa
Susan Yunusova <yunusova96.ys@gmail.com>
2020-07-03 02:17:33 +0400

    Crud Controller added for user

--- src/main/java/az/ibatech/todo/api/controller/UserController.java
+++ src/main/java/az/ibatech/todo/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package az.ibatech.todo.api.controller;
 
+import az.ibatech.todo.api.service.UserService;
 import az.ibatech.todo.db.entities.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,25 +10,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
-    private  final  UserService userService;
+    private  final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/add")
-    public ResponseEntity<?>  create(User user){
+    public ResponseEntity<?>  create(@RequestBody User user){
        log.info("creating user...");
       return userService.saveOrUpdate(user);
 
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?>  update(User user){
+    public ResponseEntity<?>  update(@RequestBody User user){
        log.info("updating user...");
       return userService.saveOrUpdate(user);
 
     }
-    @DeleteMapping("/delete")
-    public ResponseEntity<?>  update(User user){
+    @DeleteMapping("/delete/{idUser}")
+    public ResponseEntity<?>  update(@PathVariable long idUser){
        log.info("creating user...");
-      return userService.saveOrUpdate(user);
+      return userService.delete(idUser);
 
     }
 }
