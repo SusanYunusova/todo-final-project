0faa0b56bba25505698f35ca0ea87219db4d869a
ismailovafidan <fidan97ismailova@gmail.com>
2020-07-03 15:44:04 +0400

    TaskDbService has created

--- src/main/java/az/ibatech/todo/db/service/impl/TaskDBService.java
+++ src/main/java/az/ibatech/todo/db/service/impl/TaskDBService.java
@@ -1,33 +1,77 @@
 package az.ibatech.todo.db.service.impl;
 
+import az.ibatech.todo.db.entities.Task;
+import az.ibatech.todo.db.entities.User;
+import az.ibatech.todo.db.repos.TaskRepository;
 import az.ibatech.todo.db.service.MySqlDBService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class TaskDBService implements MySqlDBService {
-    @Override
-    public Optional saveUpdate(Object o) {
-        return Optional.empty();
+
+@Service
+@Slf4j
+public class TaskDBService implements MySqlDBService<Task> {
+    private final TaskRepository taskRepository;
+
+    public TaskDBService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
     }
 
     @Override
-    public boolean delete(Object o) {
-        return false;
+    public Optional<Task> saveUpdate(Task task) {
+        try {
+            log.info("trying to save task to db");
+            taskRepository.save(task);
+            return Optional.of(task);
+
+        } catch (Exception e){
+            log.error("error saving task to db {}{}",e,e);
+            return Optional.empty();
+        }
     }
 
     @Override
-    public Optional getById(long id) {
-        return Optional.empty();
+    public boolean delete(Task task) {
+        try {
+            log.info("trying to delete task from db");
+            taskRepository.delete(task);
+            return true;
+        } catch (Exception e) {
+            log.error("error from deleting task from db{}{}", e, e);
+            return false;
+        }
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public Optional<Task> getById(long id) {
+        try {
+            log.info("trying to get task by id from db");
+            Optional<Task> task = taskRepository.findByIdTask(id);
+            return task;
+        } catch (Exception e) {
+            log.error("error getting by id from Db{}{}", e, e);
+            return Optional.empty();
+        }
     }
 
     @Override
-    public List getAllBy(long id) {
+    public List<Task> getAll() {
+        try {
+            log.info("trying to getAll task from db");
+            List<Task> all = taskRepository.findAll();
+            return all;
+        } catch (Exception e) {
+            log.error("error getting all from db{}{}", e, e);
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public List<Task> getAllBy(long id) {
         return null;
     }
 }
