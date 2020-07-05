d0ce6e4fdddfd1c0659a2fa1b4345c60aeee525a
Susan Yunusova <yunusova96.ys@gmail.com>
2020-07-06 03:17:45 +0400

    LandingController.java.java added added

---
+++ src/main/java/az/ibatech/todo/api/controller/LandingController.java
@@ -0,0 +1,19 @@
+package az.ibatech.todo.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+public class LandingController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, HttpSession httpSession) {
+        log.info("going to dashboard page...");
+        return "tasks-dashboard";
+    }
+}
